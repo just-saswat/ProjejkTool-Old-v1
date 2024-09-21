b105f2e9041a946e04f1161cac0535a35732768c
SASWAT PRADHAN <saswatpradhan.python1452@gmail.com>
2024-09-21 14:56:18 +0530

    Whole Project Modified for PWA

--- sw.js
+++ sw.js
@@ -1,46 +1,41 @@
 const cacheName = 'app-v1';
 const filesToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/path/to/icon-192x192.png',
-  '/path/to/icon-512x512.png',
-  '/css/styles.css',
-  '/js/app.js'
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
 ];
 
 // Install the service worker
 self.addEventListener('install', event => {
-  console.log('Service worker installing...');
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 // Activate the service worker
 self.addEventListener('activate', event => {
-  console.log('Service worker activating...');
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
 // Fetch and cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
-
